2482858d06500a0b2c424c27b3e79169f3700be4
zhangxwww <631133544@qq.com>
2020-04-05 17:46:35 +0800

    dataset

--- dataset.py
+++ dataset.py
@@ -1,4 +1,26 @@
-import PIL
+import os
+from PIL import Image
+import numpy as np
+
 
 class Dataset:
-    def __init__(self, path='./Dataset'):
+    def __init__(self, path='./Dataset',
+                 all_list='AllImages.txt',
+                 query_list='QueryImages.txt'):
+        self.path = path
+        self.all_list = all_list
+        self.query_list = query_list
+
+    def get_all_images(self):
+        return self._read_images(self.all_list)
+
+    def get_query_images(self):
+        return self._read_images(self.query_list)
+
+    def _read_images(self, image_list):
+        with open(image_list) as f:
+            lines = f.readlines()
+        lines = filter(lambda x: len(x) > 0, lines)
+        image_names = list(map(lambda x: x.split()[0], lines))
+        all_images = list(map(lambda x: np.array(Image.open(os.path.join(self.path, x))), image_names))
+        return all_images, image_names
--- histogram.py
+++ histogram.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-_PARTITION_16 = np.array([128, 64, 128], dtype=np.uint8).reshape(-1, 1, 1)
-_PARTITION_128 = np.array([64, 32, 64], dtype=np.uint8).reshape(-1, 1, 1)
+_PARTITION_16 = np.array([128, 64, 128], dtype=np.uint8).reshape((-1, 1, 1))
+_PARTITION_128 = np.array([64, 32, 64], dtype=np.uint8).reshape((-1, 1, 1))
 
 _WEIGHT_16 = np.array([8, 2, 1], dtype=np.uint8)
 _WEIGHT_128 = np.array([32, 4, 1], dtype=np.uint8)
